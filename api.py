f746134d5d4c802ee8f8fab3351964c7892d11e3
Adrien Dimitri <amaugueret@gmail.com>
2024-04-13 15:47:01 +0200

    added examples for values

--- api.py
+++ api.py
@@ -5,10 +5,10 @@ import time
 url = "http://127.0.0.1:8000/generate"
 
 payload = json.dumps({
-  "speaker_name": "freeman",
-  "input_text": "Welcome to the Virtual Reality AI Museum, where technology and history intertwine to offer an immersive journey through the realms of artificial intelligence (AI). As you step into this digital sanctuary, prepare to be transported into a realm where the past, present, and future of AI converge.",
-  "emotion": "Calm",
-  "speed": 0.5
+  "speaker_name": "kanye",
+  "input_text": "Hello, my name is Kanye West.",
+  "emotion": "Angry",  # happy,sad,angry,dull
+  "speed": 1.0  # 1.0 - 2.0
 })
 headers = {
   'Content-Type': 'application/json'
@@ -24,7 +24,7 @@ if response.status_code == 200:
     audio_content = response.content
     
     # Save the audio to a file
-    with open("generated_audio.wav", "wb") as audio_file:
+    with open("tmp/generated_audio.wav", "wb") as audio_file:
         audio_file.write(audio_content)
         
     print("Audio saved successfully.")
